f9f959aede77ff23f14fd009875f816178373562
Liz Boika <mdaicdxl@gmail.com>
2022-07-19 11:08:02 -0400

    commit tree 8e21da07a9853bda51b104193922fc3a67f2879e

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+const hq = 42;
+
+function distanceFromHqInBlocks(pickup) {
+    let blocks = pickup - hq;
+    blocks = Math.abs(blocks);
+    return blocks;
+}
+
+function distanceFromHqInFeet(pickup) {
+    let blocks = pickup - hq;
+    blocks = Math.abs(blocks);
+    let feet = blocks * 264;
+    return feet;
+}
+
+function distanceTravelledInFeet(pickup, dropoff) {
+    let feet = (Math.abs(pickup - dropoff)) * 264;
+    return feet;
+}
+
+function calculatesFarePrice(pickup, dropoff) {
+    let feet = (Math.abs(pickup - dropoff)) * 264; 
+    if (feet < 400) {
+        let fare = 0;
+        return fare;
+    }
+    else if (feet > 399 && feet < 2001) {
+        let fare = (feet - 400) * .02;
+        return fare;
+    }
+    else if (feet > 2000 && feet < 2501) {
+        let fare = 25;
+        return fare;
+    }
+    else {
+        return 'cannot travel that far';
+    }
+}
